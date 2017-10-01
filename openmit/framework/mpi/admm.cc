41edf1079dd3d454581c92f61ddb912084f49519
diffm <zhouyong@hrbeu.edu.cn>
2017-10-01 16:40:08 +0800

    update admm to mpi framework

---
+++ openmit/framework/mpi/admm.cc
@@ -0,0 +1,70 @@
+#include "dmlc/logging.h"
+#include "dmlc/timer.h"
+#include "rabit/rabit.h"
+#include "openmit/framework/mpi/admm.h"
+#include "openmit/tools/monitor/transaction.h"
+
+namespace mit {
+
+Admm::Admm(const mit::KWArgs & kwargs) {
+  Init(kwargs);
+}
+
+void Admm::Init(const mit::KWArgs & kwargs) {
+  this->miparam_.InitAllowUnknown(kwargs);
+  admm_param_.InitAllowUnknown(kwargs);
+  cli_param_.InitAllowUnknown(kwargs);
+  mpi_worker_.reset(new MPIWorker(kwargs));
+  mpi_server_.reset(new MPIServer(kwargs, mpi_worker_->Size()));
+}
+
+void Admm::Run() {
+  rabit::Init(0, new char*[1]);
+  double startTime = dmlc::GetTime();
+  if (this->miparam_.task_type == "train") {
+    std::unique_ptr<Transaction> trans(new Transaction(0, "mpi", "train"));
+    RunTrain();
+    Transaction::End(trans.get());
+  } else if (this->miparam_.task_type == "predict") {
+    std::unique_ptr<Transaction> trans(new Transaction(0, "mpi", "predict"));
+    RunPredict();
+    Transaction::End(trans.get());
+  } else {
+    LOG(ERROR) 
+      << this->miparam_.task_type << " is not in [train, predict].";
+  }
+  double endTime = dmlc::GetTime();
+  rabit::TrackerPrintf("@node[%d] [OpenMIT-ADMM] \
+      The total time of the task %s is %g min \n", 
+      rabit::GetRank(), 
+      this->miparam_.task_type.c_str(), 
+      (endTime-startTime)/60);
+  rabit::Finalize();
+}
+
+void Admm::RunTrain() {
+  for (auto iter = 0u; iter < cli_param_.max_epoch; ++iter) {
+    LOG(INFO) << "[INFO] ============ epoch: " << iter + 1;
+    // learning 
+    mpi_worker_->Run(mpi_server_->Data(), mpi_server_->Size(), iter + 1);
+    mpi_server_->Run(mpi_worker_->Data(), mpi_worker_->Dual(), mpi_worker_->Size());
+    mpi_worker_->UpdateDual(mpi_server_->Data(), mpi_server_->Size());
+    
+    if (cli_param_.debug) { 
+      mpi_worker_->Debug(); 
+      mpi_server_->DebugTheta();
+    }
+    // metric TODO
+  }
+  if (rabit::GetRank() == 0) {
+    std::unique_ptr<dmlc::Stream> fo(
+      dmlc::Stream::Create(cli_param_.model_dump.c_str(), "w"));
+    mpi_server_->SaveModel(fo.get());
+  }
+}
+
+void Admm::RunPredict() {
+  LOG(INFO) << "Admm::RunPredict() ...";
+}
+
+} // namespace mit
---
+++ openmit/framework/mpi/server.h
@@ -0,0 +1,73 @@
+#ifndef OPENMIT_FRAMEWORK_MPI_SERVER_H_
+#define OPENMIT_FRAMEWORK_MPI_SERVER_H_
+
+#include <memory>
+
+#include "rabit/rabit.h"
+#include "openmit/common/arg.h"
+#include "openmit/common/base.h"
+#include "openmit/common/parameter/admm_param.h"
+#include "openmit/tools/dstruct/sarray.h"
+
+namespace mit {
+/*!
+ * \brief mpi global model node logic for distributed machine learning
+ */
+class MPIServer {
+  public:
+    /*! \brief constructor */
+    MPIServer(const mit::KWArgs & kwargs, 
+              const size_t max_dim);
+    
+    /*! \brief destructor */
+    ~MPIServer();
+    
+    /*! \brief initialize model node */
+    void Init(const mit::KWArgs & kwargs, 
+              const size_t max_dim);
+
+    /*! \brief update global model */
+    void Run(mit_float * local, 
+                mit_float * dual, 
+                const size_t max_dim);
+
+    /*! \brief get global model */
+    inline mit_float * Data() { 
+      return theta_.data(); 
+    }
+
+    /*! \brief global model size */
+    inline size_t Size() const { 
+      return theta_.size(); 
+    }
+
+    /*! \brief save global model */
+    void SaveModel(dmlc::Stream * fo);
+    
+    /*! \brief debug theta_ */
+    void DebugTheta();
+
+  private:
+    /*
+     * \brief global model middle value. formula:
+     *        (beta_t[j] + \rho * w_t[j])
+     */
+    void MiddleAggr(mit_float * local, 
+                    mit_float * dual,
+                    const size_t max_dim);
+
+    /* \brief global theta update */
+    void ThetaUpdate();
+
+    /*! \brief compute global model */
+    void AdmmGlobal();
+
+  private:
+    /*! \brief admm parameter */
+    mit::AdmmParam admm_param_;
+    /*! \brief global model */
+    mit::SArray<mit_float> theta_;
+}; // class MPIServer
+
+} // namespace mit
+#endif // OPENMIT_FRAMEWORK_MPI_SERVER_H_
