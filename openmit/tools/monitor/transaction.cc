ab9ea31a21b1e0b1f5cd7a84c92c1af96d3644a1
diffm <zhouyong@hrbeu.edu.cn>
2017-10-02 12:53:21 +0800

    fix leak memory

--- openmit/tools/monitor/transaction.cc
+++ openmit/tools/monitor/transaction.cc
@@ -6,8 +6,15 @@ std::stack<Transaction *> Transaction::trans_info;
 bool Transaction::_init = Transaction::Init();
 
 bool Transaction::Init() {
-  trans_info.push(new Transaction()); 
-  trans_info.pop();
+  /*
+  trans_info.push(new Transaction(99, "type", "name")); 
+  if (! trans_info.empty()) trans_info.pop();
+  // TODO error reported by valgrind 
+  ==5294== 88 bytes in 1 blocks are definitely lost in loss record 23 of 30
+  ==5294==    at 0x4C2E118: operator new(unsigned long) (vg_replace_malloc.c:333)
+  ==5294==    by 0x5314B9: mit::Transaction::Init() (in ...)
+  ==5294==    by 0x531B79: __static_initialization_and_destruction_0(int, int) (in ...)
+  */
   return true;
 }
 
@@ -15,9 +22,8 @@ Transaction * Transaction::Create(uint32_t level,
                                   std::string type, 
                                   std::string name) {
   Transaction * trans = new Transaction(level, type, name);
-  LOG(INFO) << "created transaction: <" << level << ", " 
-    << type << ", " 
-    << name << ">";
+  LOG(INFO) << "created transaction: <" 
+    << level << ", " << type << ", " << name << ">";
   trans_info.push(trans);
   return trans;
 }
@@ -30,7 +36,7 @@ void Transaction::End(Transaction * trans) {
   Transaction * trans_end = new Transaction(
       trans->Level(), trans->Type(), trans->Name());
   trans->LogTrace(trans_end);
-  trans_info.pop();
+  if (! trans_info.empty()) trans_info.pop();
   delete trans_end;
 }
 
